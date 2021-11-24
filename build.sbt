3c0b4d50099c1df26622ed72fe95a06cd4e05e30
yash-bhardwaj-22 <yash.bhardwaj@knoldus.com>
2021-11-24 09:49:25 +0530

    Commit

--- build.sbt
+++ build.sbt
@@ -4,4 +4,10 @@ version := "0.1"
 
 scalaVersion := "2.13.7"
 
-libraryDependencies += "com.github.pathikrit" %% "better-files" % "3.9.1"
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-actor" % "2.6.16",
+  "com.typesafe.akka" %% "akka-stream" % "2.6.16",
+  "com.typesafe.akka" %% "akka-http" % "10.2.6",
+  "com.typesafe.akka" %% "akka-http-spray-json" % "10.2.6",
+  "ch.megard" %% "akka-http-cors" % "1.1.2"
+)
--- src/main/scala/CRUD/fileManager.scala
+++ src/main/scala/CRUD/fileManager.scala
@@ -2,33 +2,44 @@ package CRUD
 
 import java.io._
 import scala.io.Source
+import akka.actor.ActorSystem
+import akka.actor
 
 object fileManager extends App{
-  val newFile = new File("Test1.txt")
-  val printWriter = new PrintWriter(newFile)
-  printWriter.write("Hello this is rendom text for testing")
-  printWriter.close()
-
-  read(newFile)
-  modify(newFile)
-
-  def read(file: File) = {
-    val fileSource = Source.fromFile(file)
-    for(line<-fileSource.getLines()){
-      println(line)
-    }
-    fileSource.close()
-  }
-
-  def delete(file: File) = {
-    val f1 = file
-    if(f1.exists()){
-      f1.delete()
-      println("File " + f1 + " Deleted")
-    }
-  }
-
-  def modify(file: File) = {
-    Source.fromFile(file).getLines().foreach({ line => println("*** " + line + " ***")})
-  }
+  def createDir(name:String) = {
+    val dir = new File(name)
+    if(dir.exists()) println("Directory already exists")
+    else { dir.mkdir()
+      println("Directory created") }
 }
+
+def createFile(name:String) = {
+  val newFile = new File(name)
+  if(newFile.exists()) println("File already exists")
+  else {
+    val printWriter = new PrintWriter(newFile)
+    printWriter.write("Hello this is rendom text for testing")
+    printWriter.close() }
+  }
+
+
+    def read(file: File) = {
+      val fileSource = Source.fromFile(file)
+      for(line<-fileSource.getLines()){
+        println(line)
+      }
+      fileSource.close()
+    }
+
+    def delete(file: File) = {
+      val f1 = file
+      if(f1.exists()){
+        f1.delete()
+        println("File " + f1 + " Deleted")
+      }
+    }
+
+    def modify(file: File) = {
+      Source.fromFile(file).getLines().foreach({ line => println("*** " + line + " ***")})
+    }
+  }
